629592ede414c91d3de4f4a9f1587ae03be9f223
winkt0 <238452092+winkt0@users.noreply.github.com>
2026-02-28 11:08:32 +0100

    Optimized smoothing of audio data by more elaborate prevention of faulty frequency changes

--- lib/util/audio_analyzer.dart
+++ lib/util/audio_analyzer.dart
@@ -17,15 +17,15 @@ class AudioAnalyzer {
   }
 
   static List<double> smooth(List<double> frequencies) {
-    double current = freqToNote(frequencies[0]).freq;
-    List<double> smoothed = List.filled(frequencies.length, current);
+    double currentSmoothedFreq = freqToNote(frequencies[0]).freq;
+    List<double> smoothed = List.filled(frequencies.length, currentSmoothedFreq);
     for (int i = 1; i < frequencies.length; i++) {
       double freq = freqToNote(frequencies[i]).freq;
-      if (current == freq || _isSmallerMultiple(current.round(), freq.round())) {
-        smoothed[i] = current;
+      if (isProbablySameNote(currentSmoothedFreq, freq, frequencies, i)) {
+        smoothed[i] = currentSmoothedFreq;
       } else if (i < frequencies.length - 1) {
-        current = freq;
-        smoothed[i] = current;
+        currentSmoothedFreq = freq;
+        smoothed[i] = currentSmoothedFreq;
       } else {
         smoothed[i] = freq;
       }
@@ -33,8 +33,29 @@ class AudioAnalyzer {
     return smoothed;
   }
 
+  static bool isProbablySameNote(double currentSmoothedFreq, double freq, List<double> frequencies, int i) {
+    return currentSmoothedFreq == freq ||
+        _isSmallerMultiple(currentSmoothedFreq.round(), freq.round()) || 
+        _isNoisyJump(freq, frequencies, i);
+  }
+
   static bool _isSmallerMultiple(int a, int b) {
     int rest = a > b ? a % b : b % a;
     return rest <= 2 && a > b;
+  }
+  static bool _isNoisyJump(double freq, List<double> frequencies, int i) {
+    return i + 2 < frequencies.length && _nextFrequenciesAreNotTheSame(freq, frequencies, i, 2);
+  }
+
+  static bool _nextFrequenciesAreNotTheSame(
+    double freq,
+    List<double> frequencies,
+    int i,
+    int nextAmount,
+  ) {
+    for (int testI = i; testI < i + nextAmount; testI++) {
+      if (freqToNote(frequencies[testI]).freq != freq) return true;
+    }
+    return false;
   }
 }
